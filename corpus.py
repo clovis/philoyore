cbafa20e9bce5432285ab235192ae57995f06dc7
Ricky Stewart <rbstewart@uchicago.edu>
2014-05-23 15:58:21 -0500

    Implemented distance, subcorpora for corpus class

--- corpus.py
+++ corpus.py
@@ -15,7 +15,11 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 # ========================================================================
 
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, HashingVectorizer, TfidfTransformer
+import sklearn.metrics.pairwise as dist
+import sklearn.preprocessing as pre
+import numpy as np
+import scipy as sp
 
 # A corpus is a collection of vectorized documents. To construct a corpus,
 # a collection of "documents" must be passed in; these documents will be 
@@ -43,39 +47,95 @@ class Corpus:
     # Extra input options will be passed directly to the CountVectorizer
     # or TfidfVectorizer constructor method, depending on which `strategy`
     # is selected ('count' for CountVectorizer or 'tf-idf' for TfidfVectorizer).
+    # Use 'hashingcount' as the strategy to use the `HashingVectorizer` from 
+    # the Scikit library; use 'hashingtf-idf' as the strategy to use the
+    # hashing strategy but to pipe the result into a Tf-Idf transformer.
+    # A set of subcorpora will be automatically generated according to the 
+    # groups that were passed in; they will automatically be numbered starting
+    # from 0.
     def __init__(self, groups, strategy = 'count', input = 'filename', 
-                 **options):
+                 scale = True, **kwargs):
         all_groups = sum(groups, [])
+        # Make our vectorizer
         if strategy == 'count':
-            self.vectorizer = CountVectorizer(input = input, **options)
+            self.vectorizer = CountVectorizer(input = input, dtype = np.float64,
+                                              **kwargs)
+        elif strategy == 'hashingcount' or strategy == 'hashingtf-idf':
+            self.vectorizer = HashingVectorizer(input = input, 
+                                                dtype = np.float64, **kwargs)
         elif strategy == 'tf-idf':
-            self.vectorizer = TfidfVectorizer(input = input, **options)
+            self.vectorizer = TfidfVectorizer(input = input, dtype = np.float64,
+                                              **kwargs)
         else:
             raise RuntimeError, 'Unrecognized vectorization strategy ' + \
                 strategy
+        # Vectorize
         self.vecs = self.vectorizer.fit_transform(all_groups)
+        # Transform if we need to
+        if strategy == 'hashingtf-idf':
+            self.vecs = TfidfTransformer().fit_transform(self.vecs)
+        self.vecs = self.vecs.tocsr()
+        # Scale if we need to
+        self.vecs /= self.vecs.max()
+        self.vecs *= 1000.0
+        # Load up our subcorpora
+        self.subcorpora_hash = {}
+        self.subcorpora_indices = {}
+        index = 0
+        for i, group in enumerate(groups):
+            self.subcorpora_indices[i] = np.array(range(index, 
+                                                        index + len(group)))
+            self.subcorpora_hash[i] = self.vecs[self.subcorpora_indices[i]]
+            index += len(group)
+    def subcorpus(self, key):
+        return self.subcorpora_hash[key]
+    # The input `group` is a list of 2-tuples of the following form:
+    # (SUBCORPUSKEY, INDEX)
+    # The input `key` parameter is the key that will be tied to this group, 
+    # which can be any object (probably an integer or string). For example,
+    # if we want to create a subcorpus consisting of the 0th document from
+    # the subcorpora with keys "a", "b", and "c", we might do it with this
+    # call:
+    # corpus.add_subcorpus('new', [('a', 0), ('b', 0), ('c', 0)])
+    def add_subcorpus(self, key, group):
+        a = [self.subcorpora_indices[subkey][ind] for subkey, ind in group]
+        self.subcorpora_hash[key] = self.vecs[a]
+    def del_subcorpus(self, key):
+        del self.subcorpora_hash[key]
+    # Return a list of all the subcorpus keys.
+    def subcorpora(self):
+        return self.subcorpora_hash.keys()
+    def features(self):
+        return self.vectorizer.get_feature_names()
+    def feature_idx(self, feature):
+        return self.vectorizer.vocabulary_.get(feature)
+    # X_subcorp and Y_subcorp should be subcorpus keys.
+    def distance(self, X_subcorp = 0, Y_subcorp = None, n_jobs = -1, **kwargs):
+        X = self.subcorpus(X_subcorp)
+        Y = None if Y_subcorp is None else self.subcorpus(Y_subcorp)
+        return dist.pairwise_distances(X = X, Y = Y, **kwargs)
 
 # Convert a list of files to a corpus with one subcorpus.
-def from_files(fs, **options):
-    return Corpus([fs], input = 'file', **options)
+def from_files(fs, **kwargs):
+    return Corpus([fs], input = 'file', **kwargs)
 
 # Convert a list of filenames to a corpus with one subcorpus.
-def from_filenames(fns, **options):
-    return Corpus([fns], **options)
+def from_filenames(fns, **kwargs):
+    return Corpus([fns], **kwargs)
 
 # Convert a list of strings to a corpus with one subcorpus.
-def from_strings(ss, **options):
-    return Corpus([ss], input = 'content', **options)
+def from_strings(ss, **kwargs):
+    return Corpus([ss], input = 'content', **kwargs)
 
 # Convert a list of lists of files to a corpus.
-def from_file_lists(fss, **options):
-    return Corpus(fss, input = 'file', **options)
+def from_file_lists(fss, **kwargs):
+    return Corpus(fss, input = 'file', **kwargs)
 
 # Convert a list of lists of fileneames to a corpus (the default behavior of 
 # the constructor).
-def from_filename_lists(fnss, **options):
-    return Corpus(fnss, **options)
+def from_filename_lists(fnss, **kwargs):
+    return Corpus(fnss, **kwargs)
 
 # Convert a list of lists of strings to a corpus.
-def from_string_lists(sss, *options):
-    return Corpus(sss, input = 'content', **options)
+def from_string_lists(sss, *kwargs):
+    return Corpus(sss, input = 'content', **kwargs)
